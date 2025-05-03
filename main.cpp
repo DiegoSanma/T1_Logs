8971dc4ff2608f842c89360d5a2b80ee5ee6d624
DiegoSanma <diego.sanmartin.a@ug.uchile.cl>
2025-05-03 16:09:04 -0400

    feat: Main prueba aridad ahora

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "crear_array.h"
 #include "mergesort.h"
+#include "find_alpha.cpp"
 #include <iostream>
 
 
@@ -7,23 +8,27 @@ int main(){
     int M = 50;
     size_t B = 4096; //4096kb
     //Primero, inicializo la clase que crea el arreglo
-    CrearArray creador("arreglo.bin",M,4);
+    CrearArray creador("arreglo.bin",M,60);
     std::cout << "Hola desde Docker!" << std::endl;
-    for(int i=4;i<=60;i+=4){
-        creador.setX(i);
-        for(int j=0;j<5;++j){
-            size_t largo_archivo = M*i*1024*1024;
-            //Primero creamos la secuencia de i * M numeros de 64 bits
-            creador.crearArrayN();
-            //Dsp tenemos que hacerle mergesort y quicksort, y guardar los resultados
-            MergeSort mergesort("arreglo.bin",2,largo_archivo,0,B);
-            int IOs_merge = mergesort.MergeSortN(M,B);
-            //Finalmente, borro lo que había en el archivo.bin y sigo con la próxima secuencia
-        
-        }
-
-    }
-    std::cout << "Hola desde Docker2!" << std::endl;
+    //Ahora, debo sacar la aridad usando MergeSort
+    int alfa = findOptimalArity(B,"arreglos_aridad.bin",M,60,B);
+    //Ahora usando ese alfa, debo realizar MergeSort y QuickSort
+    //Lo comento pa q cuando lo corran solo veamos primero la aridad
+    //for(int i=4;i<=60;i+=4){
+    //    creador.setX(i);
+    //    for(int j=0;j<5;++j){
+    //        size_t largo_archivo = M*i*1024*1024;
+    //        //Primero creamos la secuencia de i * M numeros de 64 bits
+    //        creador.crearArrayN();
+    //        //Dsp tenemos que hacerle mergesort y quicksort, y guardar los resultados
+    //        MergeSort mergesort("arreglo.bin",alfa,largo_archivo,0,B);
+    //        int IOs_merge = mergesort.MergeSortN(M,B);
+    //        //Finalmente, borro lo que había en el archivo.bin y sigo con la próxima secuencia
+    //    
+    //    }
+//
+    //}
+    //std::cout << "Hola desde Docker2!" << std::endl;
 
     return 0;
 }
