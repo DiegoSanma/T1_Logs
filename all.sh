45678616abaf2bd69d9df5a02ba4a2b797ba90f2
Ateuluz <lucas.zulueta.w@gmail.com>
2025-05-06 18:11:32 -0400

    feat: .sh

---
+++ all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build -t my-app .
+
+docker run -d --rm --name my-app-container my-app 
+
+# docker stop my-app-container #This command is for early stopping the container
+echo "Container is running in background. Press ENTER to stop it..."
+read
+docker stop my-app-container
